38af6d727d05c7dc0a769a16bf77acfb6a4ca919
longliang-dev <longliang2025@163.com>
2025-03-09 19:40:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,129 @@
+function load2D_bg(){
+    var background = document.getElementById("background");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            
+                    
+            background.style.backgroundPositionX = xValue * 1 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
+        })
+    },false);
+
+}
+
+function load2D_bg2(){
+    var background = document.getElementById("background2");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            
+            background.style.backgroundPositionX = xValue *1 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
+        })
+    },false);
+
+}
+
+function load2D_bg2_2(){
+    var background = document.getElementById("background2");
+    document.onmousemove=function(){
+        console.log(window.outerHeight);
+        background.style.opacity=1-(getMousePos()/window.outerHeight*2.5);
+    }
+}
+
+function getMousePos(event) {
+    var e = event || window.event;
+    var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+    var y = e.pageY || e.clientY + scrollY;
+    return y;
+}
+
+function load2D_lihui(){
+    var background = document.getElementById("lihui");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var xValue = calcValue(x, window.innerWidth);
+
+            background.style.left = xValue * 0.7-100 + "px";
+            
+        })
+    },false);
+
+}
+
+function load2D_yinghua(){
+    var background = document.getElementById("yinghua");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            background.style.backgroundPositionX = xValue * 1.5 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 1-50 ) + "px";
+        })
+    },false);
+
+}
+var i=0;
+
+function aboutMe(){
+    var slogan = document.getElementById("slogan");
+    var slogan2 = document.getElementById("slogan2");
+    if(i==0){
+        i=1;
+        slogan.style.height="100px";
+        slogan2.innerHTML="这是我们的密码哦，需付费了解，但可以偷偷告诉你，我的家慧是一个超级小吃货哦，有好吃的记得分享给我，我也要给我老婆大人买一份";
+    } else {
+        i=0;
+        slogan.style.height="50px";
+        slogan2.innerHTML="如果爱情有落款，那么一定是家慧！";
+    }
+    
+}
